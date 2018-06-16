772a6d4651314a939abf8e31453073d9e2dc92a4
jjorissen52 <jjorissen52@gmail.com>
2018-06-16 16:41:38 -0500

    added create_pg_db to thing

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-ssh skull "rm -rf deployments/* && git clone https://github.com/jjorissen52/react-wedding deployments/react_wedding"e
+ssh skull "rm -rf deployments/* && git clone https://github.com/jjorissen52/react-wedding deployments/react_wedding"
+scp wedding_api/secrets.ini skull:deployments/react_wedding/wedding_api
 ssh skull "cd deployments/react_wedding && docker-compose build"
